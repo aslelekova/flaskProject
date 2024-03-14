40a17b37e5e5b3f560b8c1d147909c93049eff39
aslelekova <144113963+aslelekova@users.noreply.github.com>
2024-03-14 21:03:31 +0300

    Registration form

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,7 @@
-# backend/app.py
-
 from flask import Flask, request, render_template
 import psycopg2
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
 
 # Функция для подключения к базе данных PostgreSQL
 def connect_to_db():
@@ -15,45 +13,51 @@ def connect_to_db():
         database='flask_app'
     )
 
-# Функция для создания таблицы names, если она не существует
-def create_names_table():
+# Функция для создания таблицы users, если она не существует
+def create_users_table():
     try:
         connection = connect_to_db()
         cursor = connection.cursor()
         cursor.execute('''
-            CREATE TABLE IF NOT EXISTS names (
+            CREATE TABLE IF NOT EXISTS users (
                 id SERIAL PRIMARY KEY,
-                name VARCHAR(100) NOT NULL
+                name VARCHAR(100) NOT NULL,
+                surname VARCHAR(100) NOT NULL,
+                email VARCHAR(100) NOT NULL,
+                login VARCHAR(100) NOT NULL
             )
         ''')
         connection.commit()
     except psycopg2.Error as e:
-        print(f'Error creating names table: {e}')
+        print(f'Error creating users table: {e}')
     finally:
         if connection:
             cursor.close()
             connection.close()
 
-# Создаем таблицу names при запуске приложения
-create_names_table()
+# Создаем таблицу users при запуске приложения
+create_users_table()
 
 # Маршрут для отображения HTML-формы
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Маршрут для сохранения имени в базе данных
-@app.route('/save_name', methods=['POST'])
-def save_name():
+# Маршрут для сохранения данных в базе данных
+@app.route('/save_data', methods=['POST'])
+def save_data():
     name = request.form['name']
+    surname = request.form['surname']
+    email = request.form['email']
+    login = request.form['login']
     try:
         connection = connect_to_db()
         cursor = connection.cursor()
-        cursor.execute('INSERT INTO names (name) VALUES (%s)', (name,))
+        cursor.execute('INSERT INTO users (name, surname, email, login) VALUES (%s, %s, %s, %s)', (name, surname, email, login))
         connection.commit()
-        return 'Name saved successfully!'
+        return '<script>alert("Data saved successfully!"); window.location.replace("/")</script>'
     except psycopg2.Error as e:
-        return f'Error saving name: {e}'
+        return f'Error saving data: {e}'
     finally:
         if connection:
             cursor.close()
